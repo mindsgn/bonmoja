9365da7b6964b9c137e4de7bd402536522741c95
mindsgn <zookid@protonmail.com>
2025-04-01 15:38:37 +0200

    transactions

--- app.vue
+++ app.vue
@@ -1,10 +1,3 @@
-<script setup>
-import { Motion } from 'motion-v'
-</script>
-
 <template>
-  <Motion :whileHover="{ scale: 1.1 }"
-  :whilePress="{ scale: 0.95 }">
-    <Button>Click me</Button>
-  </Motion>
+ <WalletCard />
 </template>
